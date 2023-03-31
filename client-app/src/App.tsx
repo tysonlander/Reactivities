9867627a0dcf3879dbb2f40a83c6a63d23e382a8
Tyson Lander <tlander@insiderealestate.com>
2023-03-31 13:18:46 -0600

    Added semantic ui

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,30 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Button, Header, List } from 'semantic-ui-react';
 
 function App() {
-  const [activities, setActivities] = useState([])
+    const [activities, setActivities] = useState([]);
 
-  useEffect(()=> {
-    axios.get('http://localhost:5000/api/activities').then(response =>{
-      console.log(response)
-      setActivities(response.data)
-    })
-  }, [])
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities').then((response) => {
+            console.log(response);
+            setActivities(response.data);
+        });
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <ul>
-        {activities.map((activity: any )=> (
-          <li key={activity.id}>{activity.title}</li>
-        ))}
-       </ul>
-      </header>
-    </div>
-  );
+    return (
+        <div >
+            <Header as="h2" icon="users" content="Reactivities" />
+
+            <List>
+                {activities.map((activity: any) => (
+                    <List.Item key={activity.id}>{activity.title}</List.Item>
+                ))}
+            </List>
+            <Button></Button>
+        </div>
+    );
 }
 
 export default App;
