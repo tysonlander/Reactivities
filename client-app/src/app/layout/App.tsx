d3c9b386001acad69d829771f58dfdb76977a841
Tyson Lander <tlander@insiderealestate.com>
2023-04-03 21:09:40 -0600

    added activies list

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import logo from './logo.svg';
 import './styles.css';
 import axios from 'axios';
-import { Button, Header, List } from 'semantic-ui-react';
+import { Button, Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
     const [activities, setActivities] = useState<Activity[]>([]);
@@ -19,15 +20,13 @@ function App() {
     }, []);
 
     return (
-        <div>
+        <>
             <NavBar />
-            <List>
-                {activities.map((activity) => (
-                    <List.Item key={activity.id}>{activity.title}</List.Item>
-                ))}
-            </List>
+            <Container style={{ marginTop: '7em' }}>
+                <ActivityDashboard activities={activities}/>
+            </Container>
             <Button></Button>
-        </div>
+        </>
     );
 }
 
