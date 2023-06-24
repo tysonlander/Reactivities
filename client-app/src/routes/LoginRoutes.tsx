136393192fd555d80ff3324cf31a23e0a3b794b0
Tyson Lander <tlander@insiderealestate.com>
2023-06-24 09:49:06 -0600

    added error pages

--- client-app/src/routes/LoginRoutes.tsx
+++ client-app/src/routes/LoginRoutes.tsx
@@ -7,11 +7,11 @@ import Loadable from 'components/Loadable';
 
 // render - login
 const AuthLogin = Loadable(lazy(() => import('pages/auth/login')));
-// const AuthRegister = Loadable(lazy(() => import('pages/auth/register')));
-// const AuthForgotPassword = Loadable(lazy(() => import('pages/auth/forgot-password')));
-// const AuthCheckMail = Loadable(lazy(() => import('pages/auth/check-mail')));
-// const AuthResetPassword = Loadable(lazy(() => import('pages/auth/reset-password')));
-// const AuthCodeVerification = Loadable(lazy(() => import('pages/auth/code-verification')));
+const AuthRegister = Loadable(lazy(() => import('pages/auth/register')));
+const AuthForgotPassword = Loadable(lazy(() => import('pages/auth/forgot-password')));
+const AuthCheckMail = Loadable(lazy(() => import('pages/auth/check-mail')));
+const AuthResetPassword = Loadable(lazy(() => import('pages/auth/reset-password')));
+const AuthCodeVerification = Loadable(lazy(() => import('pages/auth/code-verification')));
 
 // ==============================|| AUTH ROUTING ||============================== //
 
@@ -27,33 +27,33 @@ const LoginRoutes = {
             ),
             children: [
                 {
+                    path: '/',
+                    element: <AuthLogin />
+                },
+                {
                     path: 'login',
                     element: <AuthLogin />
                 },
-                // {
-                //   path: 'login',
-                //   element: <AuthLogin />
-                // },
-                // {
-                //   path: 'register',
-                //   element: <AuthRegister />
-                // },
-                // {
-                //   path: 'forgot-password',
-                //   element: <AuthForgotPassword />
-                // },
-                // {
-                //   path: 'check-mail',
-                //   element: <AuthCheckMail />
-                // },
-                // {
-                //   path: 'reset-password',
-                //   element: <AuthResetPassword />
-                // },
-                // {
-                //   path: 'code-verification',
-                //   element: <AuthCodeVerification />
-                // }
+                {
+                    path: 'register',
+                    element: <AuthRegister />
+                },
+                {
+                    path: 'forgot-password',
+                    element: <AuthForgotPassword />
+                },
+                {
+                    path: 'check-mail',
+                    element: <AuthCheckMail />
+                },
+                {
+                    path: 'reset-password',
+                    element: <AuthResetPassword />
+                },
+                {
+                    path: 'code-verification',
+                    element: <AuthCodeVerification />
+                }
             ]
         }
     ]
